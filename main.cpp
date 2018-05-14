ca24ef0788cf4e5dfe3e3e75a7463d4865b15668
Unknown <ph.huber@student.uni-tuebingen.de>
2018-05-15 00:05:57 +0200

    Added Excitement

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QTextStream out(stdout);
-    out << "Hello World!" << endl;
+    out << "Hello World!!!" << endl;
 
     return a.exec();
 }
