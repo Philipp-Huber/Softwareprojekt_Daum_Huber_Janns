3c4e9eb8ab6e6666fef3c7d517843660895b0a60
Alexander Janns <whitethunder990@gmail.com>
2018-06-17 23:43:23 +0200

    Added peptide table
    Also generalized the boolean editor slightly so it actually paints correctly in all columns and not just the preset ones...

--- Tabelle/booleaneditor.cpp
+++ Tabelle/booleaneditor.cpp
@@ -11,7 +11,8 @@ void BooleanDelegate::paint(QPainter *painter,
 {
     QStyleOptionViewItem viewItemOption(option);
     // Only do this if we are accessing the column with boolean variables.
-    if (index.column() == 12 || index.column() == 1) {
+    Qt::ItemFlags flags = index.flags();
+    if ((flags & Qt::ItemIsUserCheckable) || (flags & Qt::ItemIsEnabled)) {
       // This basically changes the rectangle in which the check box is drawn.
       const int textMargin = QApplication::style()->pixelMetric(QStyle::PM_FocusFrameHMargin) + 1;
       QRect newRect = QStyle::alignedRect(option.direction, Qt::AlignCenter,
@@ -72,57 +73,3 @@ bool BooleanDelegate::editorEvent(QEvent *event, QAbstractItemModel *model,
   // And set the new check state by calling the model's setData() function.
   return model->setData(index, state, Qt::CheckStateRole);
 }
-
-//QWidget *BooleanDelegate::createEditor(QWidget *parent,
-//                                     const QStyleOptionViewItem &option,
-//                                     const QModelIndex &index) const
-//{
-//    if(index.column() == 12)
-//    {
-//        QCheckBox* editor = new QCheckBox(parent);
-//        //connect(editor, &BooleanWidget::editingFinished, this, &BooleanDelegate::commitAndCloseEditor);
-//        editor->installEventFilter(const_cast<BooleanDelegate*>(this));
-//        return editor;
-//    } else {
-//        return QStyledItemDelegate::createEditor(parent, option, index);
-//    }
-//}
-
-//void BooleanDelegate::commitAndCloseEditor()
-//{
-//    QCheckBox* editor = static_cast<QCheckBox*>(sender());
-//    emit commitData(editor);
-//    emit closeEditor(editor);
-//}
-
-//Initialize editor with data from model
-//void BooleanDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
-//{
-//    if(index.column() == 12)
-//    {
-//        bool value = index.model()->data(index, Qt::EditRole).toBool();
-//        QCheckBox* widget = static_cast<QCheckBox*>(editor);
-//        widget->setChecked(value);
-//    } else {
-//        return QStyledItemDelegate::setEditorData(editor, index);
-//    }
-//}
-
-//Write data to model
-//void BooleanDelegate::setModelData(QWidget *editor,
-//                                 QAbstractItemModel *model,
-//                                 const QModelIndex &index) const
-//{
-//    if(index.column() == 12)
-//    {
-//        QCheckBox* widget = static_cast<QCheckBox*>(editor);
-//        model->setData(index, widget->isChecked());
-//    }
-
-//}
-
-//Make checkbox fit
-//void BooleanDelegate::updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const
-//{
-//    editor->setGeometry(option.rect);
-//}
--- Tabelle/main.cpp
+++ Tabelle/main.cpp
@@ -8,12 +8,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QWidget window;
     QTableView tableView;
+    QTableView tableViewPeptides;
 
-    //Make instance of model
+    //Make instance of models
     QStandardItemModel myModel(0);
+    QStandardItemModel peptideModel(0);
 
-    //Fill model with data (generic for now)
+    //Fill protein model with data (generic for now)
     for(int rows = 0; rows < 3; rows++){
         for(int columns = 0; columns < 13; columns++){
             if(columns == 12 || columns == 1){
@@ -42,6 +45,34 @@ int main(int argc, char *argv[])
         }
     }
 
+    //Fill peptide model with data (also generic)
+    for(int rows = 0; rows < 1; rows++){
+        for(int columns = 0; columns < 8; columns++){
+            if(columns == 1 || columns == 7){
+                //Create checkbox item
+                QStandardItem* checkbox = new QStandardItem(true);
+                //This item must not be editable, otherwise the app crashes when you double-click the item
+                checkbox->setEditable(false);
+                checkbox->setCheckable(true);
+                checkbox->setCheckState(Qt::Unchecked);
+                //Insert
+                peptideModel.setItem(rows, columns, checkbox);
+            }
+            else if (columns == 0){
+                QStandardItem* row = new QStandardItem(0);
+                row -> setData(rows + 1, Qt::DisplayRole);
+                row -> setEditable(false);
+                peptideModel.setItem(rows, columns, row);
+            }
+            else {
+                QStandardItem *item = new QStandardItem(0);
+                item->setData(0.3f, Qt::DisplayRole);
+                item->setEditable(true);
+                peptideModel.setItem(rows, columns, item);
+            }
+        }
+    }
+
     //Column headers constant for proteins
     const QStringList columnHeaders = {
         "",
@@ -60,11 +91,27 @@ int main(int argc, char *argv[])
     //Set header names
     myModel.setHorizontalHeaderLabels(columnHeaders);
 
+    //Column headers constant for peptides
+    const QStringList columnPeptides = {
+        "",
+        "",
+        "Pl",
+        "Sequence",
+        "Start",
+        "#Spectra",
+        "Confidence",
+        ""
+    };
+    peptideModel.setHorizontalHeaderLabels(columnPeptides);
+
     //list of columns with bars
     const QList<int> barList = {5,6,7,8,9,10,11};
+    const QList<int> peptideBarList = {5,6};
 
     //Link view to model
     tableView.setModel( &myModel );
+    tableViewPeptides.setModel(&peptideModel);
+
     foreach (int i, barList) {
         tableView.setItemDelegateForColumn(i, new barDelegate);
     }
@@ -73,9 +120,21 @@ int main(int argc, char *argv[])
     tableView.setItemDelegateForColumn(1, new BooleanDelegate);
     tableView.setEditTriggers(QAbstractItemView::DoubleClicked | QAbstractItemView::SelectedClicked);
 
+    foreach(int i, peptideBarList){
+        tableViewPeptides.setItemDelegateForColumn(i, new barDelegate);
+    }
+
+    tableViewPeptides.setItemDelegateForColumn(7, new BooleanDelegate);
+    tableViewPeptides.setItemDelegateForColumn(1, new BooleanDelegate);
+    tableViewPeptides.setEditTriggers(QAbstractItemView::DoubleClicked | QAbstractItemView::SelectedClicked);
+
     //Display
-    tableView.setWindowTitle("Bars and Checkboxes");
-    tableView.show();
+    tableView.setParent(&window);
+    tableView.setGeometry(0, 0, 1320, 250);
+    tableViewPeptides.setParent(&window);
+    tableViewPeptides.setGeometry(0, 253, 820, 250);
+    window.setWindowTitle("Bars and Checkboxes");
+    window.show();
 
     return a.exec();
 }
